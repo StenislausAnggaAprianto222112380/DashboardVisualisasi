7243716c438d39fb24943c2f040eb5e5eced458d
StenislausAnggaAprianto222112380 <222112380@stis.ac.id>
2025-08-05 14:38:30 +0700

    Update dashboard_unmet_need.py

--- dashboard_unmet_need.py
+++ dashboard_unmet_need.py
@@ -111,8 +111,8 @@ kab_terendah = gdf.loc[gdf["unpkpd"].idxmin()]
 
 # BUKA KONTENER LUAR DENGAN BACKGROUND HIJAU MUDA
 st.markdown("""
-<div style="background-color:#DBE4C9;padding:30px;border-radius:15px;margin-bottom:20px">
-    <h2 style="text-align:center;margin-top:0;margin-bottom:30px;">Statistik UNPKPD</h2>
+<div style="background-color:#DBE4C9;padding:6x;border-radius:3px;margin-bottom:4px">
+    <h2 style="text-align:center;margin-top:0;margin-bottom:6px;">Statistik UNPKPD</h2>
 """, unsafe_allow_html=True)
 
 # --- 4 KOLOM KARTU SAJA ---
